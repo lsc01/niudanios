41e5dd58446fe4a22d9fa285a19527b8315638e7
lanshichang <942776524@qq.com>
2018-08-22 23:20:23 +0800

    Initial Commit

---
+++ niudanios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  niudanios
+//
+//  Created by lsc on 2018/8/22.
+//  Copyright © 2018年 lsc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ niudanios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  niudanios
+//
+//  Created by lsc on 2018/8/22.
+//  Copyright © 2018年 lsc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
